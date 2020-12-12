92c9feaf9bf2180e9ba6cf478639260974d415fb
rimlol <48836043+rimlol@users.noreply.github.com>
2020-12-12 23:13:13 +0300

    try static

--- edtech_market/settings.py
+++ edtech_market/settings.py
@@ -15,7 +15,9 @@ from pathlib import Path
 from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -178,6 +180,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'edtech_market/static')
