a50db70e3f60ae129b45aff31a43d9a484c36da7
rimlol <48836043+rimlol@users.noreply.github.com>
2020-12-19 20:51:48 +0300

    dc

---
+++ edtech_market/local_prod_settings.py
@@ -0,0 +1,17 @@
+
+local = False
+debug = False
+local_vars = {
+
+    'SECRET_KEY' : 'b@e(89(q6%4oxmquv!^fh3j^p$zx@gygqp)&_1=ap%7hxiya*p',
+    'EMAIL_HOST_USER':'info@edtech.reviews',
+    'EMAIL_HOST_PASSWORD':'&t7f.vd4XCP$i2%',
+    'USE_S3':	True,
+    'AWS_ACCESS_KEY_ID':	'AKIA4IST2PP6KT2H7A7X',
+    'AWS_SECRET_ACCESS_KEY':	'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',
+    'AWS_STORAGE_BUCKET_NAME':	'edtech-reviews-media',
+
+
+
+}
+
